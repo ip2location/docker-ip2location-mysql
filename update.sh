f19797ea275fe14dc381a2167bb5595068c8ff57
IP2Location <support@ip2location.com>
2020-09-22 14:34:02 +0800

    Fixed file size checks.

--- update.sh
+++ update.sh
@@ -29,7 +29,7 @@ wget -O database.zip -q --user-agent="Docker-IP2Location/MySQL" http://www.ip2lo
 
 [ ! -z "$(grep '5 TIMES' database.zip)" ] && error "[QUOTA EXCEEDED]"
 
-[ $(wc -c < database.zip) -lt 102400 ] && error "[FILE CORRUPTED]"
+[ $(wc -c < database.zip) -lt 512000 ] && error "[FILE CORRUPTED]"
 
 success "[OK]"
 
